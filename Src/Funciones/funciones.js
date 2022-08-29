f23fc3dcbf2703e6d0349cdda5a3c0a62f68952d
Mauricio Hernandez <mauricioantoni@gmail.com>
2022-08-29 09:54:42 -0500

    added the Functions folder to work with functions in the next class

---
+++ Src/Funciones/funciones.js
@@ -0,0 +1,3 @@
+export const sumaDosNumeros = (a, b) => {
+  return a + b;
+};
--- Src/ejemplo.js
+++ Src/ejemplo.js
@@ -1,13 +1,16 @@
+import { sumaDosNumeros } from "./Funciones/funciones.js";
+
 let peliculas = [
   { numero: 1, genero: "terror", titulo: "it" },
   { numero: 2, genero: "humor", titulo: "risas" },
   { numero: 3, genero: "Suspenso", titulo: "No corras" },
   { numero: 4, genero: "Drama", titulo: "Amor mio" },
   { numero: 4, genero: "miedo", titulo: "bye" },
-
 ];
-console.log("otro cambio")
+console.log("otro cambio");
 let wrapMovies = document.getElementById("wrapMovies");
 let templateMovies = peliculas.forEach((item) => {
   wrapMovies.innerHTML += `<div class="movie"><p>${item.numero}</p><p>${item.genero}</p><p>${item.titulo}</p></div>`;
 });
+
+console.log("la suma es", sumaDosNumeros(5, 4));
